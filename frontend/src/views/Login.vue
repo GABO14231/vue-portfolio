677e557ed57550e6e5a7f6f07addad425be529a2
GABO1423 <176758048+gabo14231@users.noreply.github.com>
2025-07-27 17:36:54 -0400

    Frontend: Add Login Page

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,152 @@
+<template>
+    <div class="login-container">
+        <h2>Login</h2>
+        <form @submit.prevent="handleSubmit">
+            <input type="text" name="identifier" placeholder="Username or Email" v-model="input.identifier" required />
+            <div class="password-wrapper">
+                <input :type="showPassword ? 'text' : 'password'" name="password" placeholder="Password"
+                    v-model="input.password" required />
+                <button type="button" @click="togglePasswordVisibility" class="toggle-password">
+                    <font-awesome-icon :icon="showPassword ? 'eye-slash' : 'eye'" />
+                </button>
+            </div>
+            <button type="submit" class="login-button">Log In</button>
+        </form>
+        <Modal :message="message" :buttons="[{ label: 'Close', action: clearMessage }]"></Modal>
+        <div class="login-links">
+            <router-link to="/">Back to Home</router-link> |
+            <router-link to="/register">Register</router-link> |
+            <router-link to="/recoverpass">Forgot Password?</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+import {defineComponent, ref} from 'vue';
+import {useRouter} from 'vue-router';
+import Modal from '@/components/Modal.vue';
+import {authService} from '@/services/AuthService';
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
+
+export default defineComponent({name: 'LoginPage', components: {Modal, FontAwesomeIcon},
+    setup()
+    {
+        const router = useRouter();
+        const message = ref('');
+        const showPassword = ref(false);
+        const input = ref({identifier: '', password: ''});
+
+        const togglePasswordVisibility = () => {showPassword.value = !showPassword.value;};
+        const clearMessage = () => {message.value = '';};
+
+        const handleSubmit = async () =>
+        {
+            try
+            {
+                const response = await authService.login(input.value.identifier, input.value.password);
+                if (response.status === 'success')
+                {
+                    message.value = response.message;
+                    router.push('/dashboard');
+                }
+                else message.value = response.message;
+            }
+            catch (error) {message.value = error.message;}
+            finally {input.value.password = '';}
+        };
+
+        return {input, message, showPassword, togglePasswordVisibility, clearMessage, handleSubmit};
+    }
+});
+</script>
+
+<style scoped>
+.login-container
+{
+    max-width: 600px;
+    margin: 80px auto;
+    padding: 20px;
+    background-color: #2a2c30;
+    border-radius: 8px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.login-container h2
+{
+    color: #4A90E2;
+    margin-bottom: 15px;
+}
+
+.login-container form
+{
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+}
+
+.login-container input
+{
+    width: 96%;
+    padding: 12px;
+    border: 2px solid;
+    border-color: transparent;
+    border-radius: 5px;
+    font-size: 16px;
+    background-color: #1f2023;
+    color: #dadada;
+    transition: border 0.3s ease-in-out;
+}
+
+.login-container input:focus
+{
+    border-color: #4A90E2;
+    outline: none;
+}
+
+.login-button
+{
+    background-color: #4A90E2;
+    color: white;
+    padding: 12px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 18px;
+    transition: 0.3s ease;
+}
+
+.login-button:hover {background-color: #357ABD;}
+.login-links {margin-top: 15px;}
+
+.login-links a
+{
+    color: #4A90E2;
+    font-weight: bold;
+    text-decoration: none;
+    transition: color 0.3s ease;
+}
+
+.login-links a:hover {color: #ddd;}
+.password-wrapper {position: relative;}
+
+.toggle-password
+{
+    position: absolute;
+    right: 1px;
+    top: 50%;
+    transform: translateY(-50%);
+    background: transparent;
+    color: #dadada;
+    border: none;
+    cursor: pointer;
+    font-size: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.toggle-password:hover {color: #4A90E2;}
+</style>
